bd5b060490045f5bbbd4b097d821d3ccc711d8d4
Pankaz-verma <itsverma1192 @gmail.com>
2026-06-23 12:17:42 +0530

    logging and exception

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+hyp_E_dot = '-e .'
+def get_requirements(file_path:str)-> List[str]:
+    '''This function will return the list of requirements
+    '''
+    requirements =[]
+    with open(file_path) as file_obj:
+        requirements= file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if hyp_E_dot in requirements:
+            requirements.remove(hyp_E_dot)
+
+
+setup(
+    name = 'Thyroid_project',
+    version='0.0.1',
+    author='Pankaj Verma',
+    author_email='heysynorlx@gmail.com',
+    packages= find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
